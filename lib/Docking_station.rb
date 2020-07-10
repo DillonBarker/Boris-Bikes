37d28930f668b43154795a4c2751fdb317e1e6c8
DillonBarker <barker.dill98@gmail.com>
2020-07-10 15:26:00 +0100

    chapter 15 completed

--- lib/Docking_station.rb
+++ lib/Docking_station.rb
@@ -8,12 +8,21 @@ class DockingStation
   end
 
   def release_bike
-    raise "There are no bikes" if @bikes.count == 0
+    raise "There are no bikes" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise "There are no spaces available" if @bikes.count == 20
+    raise "There are no spaces available" if full?
     @bikes << bike
+  end
+  
+private
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.count == 0
   end
 end
